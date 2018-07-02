61da3e6a69b8290e6a1b22fbd2b100519b80e8ab
Fanweifu <137951918@qq.com>
2018-07-02 15:54:10 +0800

    version

--- ImageCon.cpp
+++ ImageCon.cpp
@@ -225,7 +225,20 @@ void waterTest() {
 }
 
 void printVersion() {
-    const GLubyte* renderer = glGetString(GL_RENDERER);    const GLubyte* vendor = glGetString(GL_VENDOR);    const GLubyte* version = glGetString(GL_VERSION);    const GLubyte* glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);    GLint major, minor;    glGetIntegerv(GL_MAJOR_VERSION, &major);    glGetIntegerv(GL_MINOR_VERSION, &minor);    printf("GL Vendor : %s\n", vendor);    printf("GL Renderer : %s\n", renderer);    printf("GL Version(string) : %s\n", version);    printf("GL Version(integer) : %d.%d\n", major, minor);    printf("GLSL Version : %s\n", glslVersion);
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* vendor = glGetString(GL_VENDOR);
+    const GLubyte* version = glGetString(GL_VERSION);
+    const GLubyte* glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    GLint major, minor;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+    printf("GL Vendor : %s\n", vendor);
+    printf("GL Renderer : %s\n", renderer);
+    printf("GL Version(string) : %s\n", version);
+    printf("GL Version(integer) : %d.%d\n", major, minor);
+    printf("GLSL Version : %s\n", glslVersion);
 
 }
 
